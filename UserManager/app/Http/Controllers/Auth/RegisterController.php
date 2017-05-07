cd558bebd07fe5c865afebcad762e35c0686c7c2
johnjqc <sir.jota@yahoo.com>
2017-05-07 15:24:11 -0500

    avance envio mail de registro y activacion

--- UserManager/app/Http/Controllers/Auth/RegisterController.php
+++ UserManager/app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Model\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Utilities\ActivationHelper;
 
 class RegisterController extends Controller
 {
@@ -60,12 +61,16 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return User
      */
-    protected function create(array $data)
-    {
-        return User::create([
+    protected function create(array $data) {
+        $role = Role::where('name', '=', 'customer')->first();
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'activated' => false,
         ]);
+        $user->attachRole($role);
+        $this->initiateEmailActivation($user);
+        return $user;
     }
 }
